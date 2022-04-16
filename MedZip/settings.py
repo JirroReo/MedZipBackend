b80dac96184d5047b064de803c1ecbb94614c8d4
Mica56 <tayotomichael18@gmail.com>
2022-04-16 23:10:41 +0800

    Added cors-headers

--- MedZip/settings.py
+++ MedZip/settings.py
@@ -51,9 +51,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -61,6 +63,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+   'http://localhost:8080',
+   'https://localhost:8080'
 ]
 
 ROOT_URLCONF = 'MedZip.urls'
