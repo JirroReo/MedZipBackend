1e6a213c1b2e82823bb26cb71a7f2cae5d6850f8
Mica56 <tayotomichael18@gmail.com>
2022-04-18 23:27:52 +0800

    Changes in AccountCreateAPIView

--- accounts/views.py
+++ accounts/views.py
@@ -48,6 +48,11 @@ class AccountCreateAPIView(CreateAPIView):
   model = Account
   serializer_class = AccountSerializer
 
-  def post(self, request, **kwargs):
-    response = super(AccountCreateAPIView, self).create(request, **kwargs)
-    return response
+  def create(self, request, **kwargs): 
+    instance = super(AccountCreateAPIView, self).create(request, **kwargs) 
+    instance.username = request.get('username')
+    instance.first_name = request.get('first_name')
+    instance.last_name = request.get('last_name')
+    instance.password = request.get('first_name')
+    instance.save()
+    return instance
